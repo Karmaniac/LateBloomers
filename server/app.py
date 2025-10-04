5124836ee9d0b56bee8baaa14290f4b1ba47e2ce
Jonah Lutchmedial <jonahlutch@gmail.com>
2025-10-04 13:16:04 -0300

    add flask api

--- server/app.py
+++ server/app.py
@@ -1,17 +1,93 @@
-# TODO:
+from flask import Flask, jsonify, request
+from models import DataQuery
+from FileManager import checkCache
 
 from dotenv import load_dotenv
 import os
+import json
+from utils.utils import authenticate_service_account
 
-from endpoints.data_import import fetch_data_year
-from utils.utils import authenticate_service_account, print_json
+from flasgger import Swagger
 
 load_dotenv()
 
 PRIVATE_KEY_PATH = os.getenv('PRIVATE_KEY_PATH')
 SERVICE_ACCOUNT = os.getenv('SERVICE_ACCOUNT')
 
-authenticate_service_account(SERVICE_ACCOUNT, PRIVATE_KEY_PATH)
-data = fetch_data_year(37.7749, -122.4194, '2021')
 
-print_json(data)
+authenticate_service_account(SERVICE_ACCOUNT, PRIVATE_KEY_PATH)
+
+app = Flask(__name__)
+swagger = Swagger(app)
+
+@app.route('/getData', methods=['POST'])
+def returnData():
+    """Endpoint to get data based on latitude, longitude, and year.
+    ---
+    post:
+      description: Get data based on latitude, longitude, and year.
+      requestBody:
+        required: true
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                latitude:
+                  type: number
+                  example: 34.0522
+                longitude:
+                  type: number
+                  example: -118.2437
+                year:
+                  type: integer
+                  example: 2023
+      responses:
+        200:
+          description: Data retrieved successfully.
+          content:
+            application/json:
+              schema:
+                type: object
+        400:
+          description: Invalid input.
+      
+    """
+    if request.is_json:
+        data = DataQuery.model_validate_json(json.dumps(request.get_json()))
+        returnJson = checkCache(data, 0)
+        
+        return jsonify(returnJson), 200
+
+    return jsonify({"error": "Request must be JSON"}), 400
+
+@app.route('/getDataFollowup')
+def returnFollowup():
+    """Endpoint to get follow-up data.
+    ---
+    get:
+      description: Get follow-up data.
+      responses:
+        200:
+          description: Follow-up data retrieved successfully.
+          content:
+            application/json:
+              schema:
+                type: object
+        400:
+          description: Invalid input.
+    """
+    #TODO await the followupdata
+    #followup = call full year range
+    
+    temp = 1 #just for getting rid of errors
+    
+    #addCache(followup)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True) # debug=True enables auto-reloading and debugger
