216e16507760a88a311fa45223fba691f8950d93
CalebBrett <calebbrett44@gmail.com>
2025-10-05 13:45:34 -0300

    Demo frontend

--- client/late-bloomers/src/Chart.jsx
+++ client/late-bloomers/src/Chart.jsx
@@ -17,31 +17,64 @@ const flower = ({ x, y, path, size }) => {
 	path.closePath();
 };
 
-function Chart() {
-	const customItems = ["Jun", "Jul", "Aug", "Sep"];
+function Chart({ data }) {
+	const customItems = ["2023-06-28", "2023-06-30", "2023-07-03", "2023-07-05", "2023-07-08", "2023-07-10", "2023-07-10", "2023-07-18", "2023-07-23", "2023-07-25", "2023-07-30"];
+	// console.log("hi")
+	// console.log(data);
+	// if (data != undefined) {
+	// setChartOptions((chartOptions.data) => data)
+	// };
 
 	const [chartOptions, setChartOptions] = useState({
+
 		// Data: Data to be displayed in the chart
-		data: [
-			{ month: 'Jan', avgTemp: 2.3, iceCreamSales: 162000 },
-			{ month: 'Feb', avgTemp: 2.3, iceCreamSales: 162000 },
-			{ month: 'Mar', avgTemp: 6.3, iceCreamSales: 302000 },
-			{ month: 'Apr', avgTemp: 6.3, iceCreamSales: 302000 },
-			{ month: 'May', avgTemp: 16.2, iceCreamSales: 800000 },
-			{ month: 'Jun', avgTemp: 16.2, iceCreamSales: 800000 },
-			{ month: 'Jul', avgTemp: 22.8, iceCreamSales: 1254000 },
-			{ month: 'Aug', avgTemp: 16.2, iceCreamSales: 800000 },
-			{ month: 'Sep', avgTemp: 14.5, iceCreamSales: 950000 },
-			{ month: 'Oct', avgTemp: 16.2, iceCreamSales: 800000 },
-			{ month: 'Nov', avgTemp: 8.9, iceCreamSales: 200000 },
-			{ month: 'Dec', avgTemp: 8.9, iceCreamSales: 200000 },
-		],
+		data:
+			[
+				{ nvdi: 0.17878426698450536, month: "2023-05-01" },
+				{ nvdi: 0.17052881758764113, month: "2023-05-04" },
+				{ nvdi: 0.08307045215562565, month: "2023-05-06" },
+				{ nvdi: 0.15404168784951702, month: "2023-05-14" },
+				{ nvdi: 0.15463917525773196, month: "2023-05-16" },
+				{ nvdi: 0.16363636363636364, month: "2023-05-19" },
+				{ nvdi: 0.24566088117489987, month: "2023-06-03" },
+				{ nvdi: 0.30919623461259954, month: "2023-06-05" },
+				{ nvdi: 0.33123425692695213, month: "2023-06-08" },
+				{ nvdi: 0.394524959742351, month: "2023-06-10" },
+				{ nvdi: 0.4219590958019376, month: "2023-06-13" },
+				{ nvdi: 0.49508196721311476, month: "2023-06-15" },
+				{ nvdi: 0.7479161404395049, month: "2023-06-28" },
+				{ nvdi: 0.7478060046189376, month: "2023-06-30" },
+				{ nvdi: 0.6923842707814833, month: "2023-07-03" },
+				{ nvdi: 0.754601226993865, month: "2023-07-05" },
+				{ nvdi: 0.776084407971864, month: "2023-07-08" },
+				{ nvdi: 0.6426764773511152, month: "2023-07-10" },
+				{ nvdi: 0.7565485362095532, month: "2023-07-18" },
+				{ nvdi: 0.7342657342657343, month: "2023-07-23" },
+				{ nvdi: 0.6665046183762762, month: "2023-07-25" },
+				{ nvdi: 0.5659490225191784, month: "2023-07-30" },
+				{ nvdi: 0.42501481920569056, month: "2023-08-02" },
+				{ nvdi: 0.28688879273041973, month: "2023-08-07" },
+				{ nvdi: 0.19030792529025745, month: "2023-08-24" },
+				{ nvdi: 0.20112676056338027, month: "2023-08-27" },
+				{ nvdi: 0.13518273888154997, month: "2023-08-29" },
+				{ nvdi: 0.1274542429284526, month: "2023-09-06" },
+				{ nvdi: 0.13614103819784526, month: "2023-09-11" },
+				{ nvdi: 0.13515655965120887, month: "2023-09-16" },
+				{ nvdi: 0.14650698602794412, month: "2023-09-18" },
+				{ nvdi: 0.12912912912912913, month: "2023-09-26" },
+				{ nvdi: 0.13636363636363635, month: "2023-09-28" },
+				{ nvdi: 0.1332398316970547, month: "2023-10-06" },
+				{ nvdi: 0.12841670964414648, month: "2023-10-08" },
+				{ nvdi: 0.14271047227926079, month: "2023-10-11" },
+				{ nvdi: 0.13675213675213677, month: "2023-10-16" },
+				{ nvdi: 0.13226452905811623, month: "2023-10-21" }
+			],
 		// Series: Defines which chart type and data to use
 		series: [{
 			type: "line",
 			xKey: "month",
-			yKey: "avgTemp",
-			yName: "Temperature",
+			yKey: "nvdi",
+			yName: "Spring Wheat - Regina, Saskatchewan",
 			marker: {
 				shape: "diamond",
 				size: 24,
@@ -82,7 +115,7 @@ function Chart() {
 				position: "left",
 				type: "number",
 				title: {
-					text: "Temperature (°C)",
+					text: "NVDI",
 				},
 				// crossLines: [
 				// {
--- client/late-bloomers/src/Dashboard.jsx
+++ client/late-bloomers/src/Dashboard.jsx
@@ -1,29 +1,98 @@
 import { useState } from 'react'
 import Chart from './Chart.jsx'
+import chart from './chart.png';
+
 
 function Dashboard() {
 	const [count, setCount] = useState(0);
+	const [year, setYear] = useState(2023);
+	const [location, setLocation] = useState([50.17272365327966, -104.04759247548621]);
+	const [nvdi, setNvdi] = useState()
+
+
+	// const data = [];
+	// get_data(year, location).then(data => { console.log(data.ndvi); console.log(data.dates); setNvdi((nvdi) => { data.date, data.ndvi }) });
 
 	return (
 		<div id='dashboard'>
 			<div id='past'>
 				<div id="map-container">
-					<iframe src='https://bloomwatch-nasasapceapps.projects.earthengine.app/view/late-bloomers' title='Google Earth Engine Map'></iframe>
+					<iframe src='https://bloomwatch-nasasapceapps.projects.earthengine.app/view/car-with-agri-yeilds' title='Google Earth Engine Map'></iframe>
 				</div>
 				<div className='graph'>
-					<Chart />
+					<Chart data={nvdi} />
 				</div>
 			</div>
 			<div id='future'>
-				<h1>Future</h1>
-				<div className="card">
-					<button onClick={() => setCount((count) => count + 1)}>
-						count is {count}
-					</button>
+				<form action="">
+					<select name="years" id="years">
+						<option value="2023">2020</option>
+						<option value="2023">2021</option>
+						<option value="2023">2022</option>
+						<option value="2023" selected="selected">2023</option>
+						<option value="2024">2024</option>
+						<option value="2025">2025</option>
+					</select>
+					<select name="location" id="location">
+						<option value="-104.04759247548621, 50.17272365327966">Regina, Saskatchewan</option>
+						<option value="0, 0">Calgary, Alberta</option>
+						<option value="0, 0">Winnipeg, Manitoba</option>
+					</select>
+					<input type="submit" value="Submit" />
+				</form>
+				<div className='card'>
+					<div className='card_content'>
+						<ul>
+							<h3>Current readings:</h3>
+							<li>Temperature: 7.321 C</li>
+							<li>Soil temp: 9.825 C</li>
+							<li>Surface pressure: 96811 Pa</li>
+							<li>NDVI: 0.137</li>
+							<li>Total evaporation: -0.0023870 m</li>
+							<li>Total precipitation: 1.246942 m</li>
+						</ul>
+
+					</div>
+				</div>
+				<div className='card'>
+					<div className='card_content'><img src={chart} alt="Chart" /></div>
 				</div>
 			</div>
 		</div>
 	)
+}
+
+// Get data from the server
+// Return promise
+async function get_data(year, location) {
+	const server_endpoint = import.meta.env.VITE_SERVER;
+
+	try {
+		// Post number of planets and receive list of n planets
+		const response = await fetch(server_endpoint + "/getData", {
+			method: "POST",
+			body: JSON.stringify({
+				"latitude": location[0],
+				"longitude": location[1],
+				"year": year,
+			}),
+			headers: {
+				"Content-type": "application/json"
+			}
+		});
+
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`);
+		}
+
+		const result = await response.json();
+		console.log(result);
+
+		return result;
+
+	} catch (error) {
+		console.error(error.message);
+	}
 }
 
 export default Dashboard
